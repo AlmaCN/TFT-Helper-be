ef183f49c628d559810112d8ab08909957eea709
almacaciula <almacaciulanegrea@gmail.com>
2024-04-21 15:51:43 +0200

    update

--- src/main/java/com/cnade/betfthelper/entity/dto/FirstMatchDTOIn.java
+++ src/main/java/com/cnade/betfthelper/entity/dto/FirstMatchDTOIn.java
@@ -8,10 +8,12 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class FirstMatchDTOIn {
+
     private String league;
     private int leagueTier;
     private boolean victory;
     private int lpMatch;
     private String compName;
     private String playerName;
+    
 }
